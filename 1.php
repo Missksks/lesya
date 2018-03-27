c55fc9e8ded290b56f7eec4a2b9d0ef0d43608cd
lesya <ya.zhiraf2012@yandex.ru>
2018-03-27 13:30:10 +0300

    djskds

---
+++ 1.php
@@ -0,0 +1,43 @@
+
+<?php
+
+$out="";
+for($i=0;$i<=10;$i++){
+	
+	$out.="<tr>\n";
+	for($j=0;$j<=10;$j++){
+		
+		$styles = "";
+		
+		if($i==$j){
+			$styles = "background: #5bc75b;color:yellow;";	
+		}
+
+
+		$result= $i*$j;
+
+		if($i==0){
+			$result = $j*1;
+			$styles = "background: #476df9;color:red;";
+		}
+		elseif($j==0){
+			$result=$i*1;
+			$styles = "background: #476df9;color:red;";
+		}
+
+		if($result==0){
+			$result = '';
+			$styles = "background: #476df9;color:red;";
+		}
+
+		$out.="<td width='30'  style='".$styles."''>".$result."</td>\n";
+
+		unset($styles);
+
+	}
+	$out.="</tr>\n";
+	// echo $out;
+
+}
+require "maket.tpl";
+?>
---
+++ maket.tpl
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Таблица Пифагора</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	
+
+</head>
+<body>
+	<h1 align="center">Таблица Пифагора</h1>
+	<table class="style-table">
+		<?=$out?>
+		</table>
+
+</body>
+</html>
